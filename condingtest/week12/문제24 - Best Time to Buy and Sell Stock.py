3f9ce049934d281a86f3211638d40e40c362a23a
inwoo <jiw413@naver.com>
2024-10-24 21:24:14 +0900

    문제24 - Best Time to Buy and Sell Stock.py

---
+++ condingtest/week12/문제24 - Best Time to Buy and Sell Stock.py
@@ -0,0 +1,33 @@
+"""
+출처 : Leet Code
+번호 : 121
+난이도 : Medium
+제목 : Best Time to Buy and Sell Stock
+"""
+
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        result = 0
+        max_price = 0
+        for i in range(len(prices)-1, 0, -1):
+            max_price = max(max_price, prices[i])
+            result = max(result, max_price-prices[i-1])
+        return result
+
+
+
+# ==============================================
+CaseCount = 2
+case = []
+case.append([7,1,5,3,6,4])
+case.append([7,6,4,3,1])
+for i in range(CaseCount):
+    print(f"=== Case {i+1} ===")
+    print("prices:", case[i])
+    print("result:", Solution().maxProfit(case[i]))
+    print()
